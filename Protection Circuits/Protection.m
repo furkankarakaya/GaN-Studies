1945cc839b83a52ef6669bbb170edc03bf0fd26e
Ayasli\Furkan <furkan.karakaya@metu.edu.tr>
2018-11-06 20:16:09 +0300

    The protection circuit is corrected.

--- Protection Circuits/Protection.m
+++ Protection Circuits/Protection.m
@@ -3,14 +3,10 @@
 Rpath = 0.243e-3;
 Lpath = 1.11e-9;
 C = 1e-9;
-% Rc = 1e3;
+Rc = 4.5e3;
 
-hold on
-for Rc = 880:3620:4500
-G = tf([-1*Lpath -1*Rpath],[Lpath*C C*(Rpath + Rc) 1]);
+G = tf([Lpath Rpath],[Lpath*C C*(Rpath + Rc) 1]);
 bode(G)
-end
-legend('880 ohm','4.5 kohm')
 hold off
 
 
