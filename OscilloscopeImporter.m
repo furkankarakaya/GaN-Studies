841f6b490c81d50f23ca89e1ffcae59e542f48fd
Ayasli\Furkan <furkan.karakaya@metu.edu.tr>
2020-03-07 11:43:51 +0300

    SCP Tests were conducted for 200ns fault duration. The SC behavior is the same with 100ns faulty case.

--- OscilloscopeImporter.m
+++ OscilloscopeImporter.m
@@ -1,21 +1,24 @@
 figure
 hold all
-Vsense400f = lowpass(Vsense400,15e6,5e9);
-Vsense350f = lowpass(Vsense350,15e6,5e9);
-Vsense300f = lowpass(Vsense300,15e6,5e9);
+%Vsense100af = lowpass(Vsense100a,45e6,5e9);
+% Vsense100bf = lowpass(Vsense100b,15e6,5e9);
+Vsense400f = lowpass(Vsense400,45e6,5e9);
+%Vsense300f = lowpass(Vsense300,45e6,5e9);
+%Vsense200f = lowpass(Vsense200,45e6,5e9);
 yyaxis left
-ylabel('GS voltage (V)')
-plot(Time,VgsBot300,'LineWidth',2);
-
-% legend('300V','350V','400V')
+plot(Time,VgsBot_200ns,'LineWidth',2);
+% plot(Time,VgsTop_200ns,'LineWidth',2);
+ylabel('Vgs Voltage (V)')
+% legend('Bot Switch','Top Switch')
 yyaxis right
-ylabel('Digital Signals')
-plot(Time,SCTrig300,'LineWidth',2,'Color','g');
-plot(Time,STO300,'LineWidth',2);
-legend('GS Voltage','SC Input','STO Command')
+ylabel('Induced Voltage (V)')
+plot(Time,Vsense400_200ns,'LineWidth',2,'Color','r');
+plot(Time,Vsense400f,'LineWidth',2,'Color','g');
+% plot(Time,Vsense200f,'LineWidth',2,'Color','k');
+legend('Vgs','200ns','100ns')
 %plot(time,Vsense150,'LineWidth',2);
 %plot(time,Vsense200,'LineWidth',2);
 % plot(time,Vtrig200,'LineWidth',2);
 xlabel('time (s)')
-title('SCP Timing')
+title('Induced voltages for different SC durations under 400V')
 
