5a0f912d0228a0cfd590faff6114a2aa14ca697d
Ayasli\Furkan <furkan.karakaya@metu.edu.tr>
2020-04-18 19:22:21 +0300

    Test results are added to weekly report.

--- OscilloscopeImporter.m
+++ OscilloscopeImporter.m
@@ -1,36 +1,21 @@
 
-%Vsense100af = lowpass(Vsense100a,45e6,5e9);
-to = 6300;
-from = 1500;
+
 
 figure
 set(gcf, 'Position',  [100, 100, 600, 400])
 hold all
-%plot(2e-10*(from:to), VrcoutSCP350(from:to),'LineWidth',1.5)
-plot(2e-10*(from:to), VrefSCP300(from:to),'LineWidth',1.5)
-plot(2e-10*(from:to), VfiltoutSCP300(from:to),'LineWidth',1.5)
-%plot(2e-10*(from:to), VscpDPT350(from:to),'LineWidth',1.5)
-%plot(2e-10*(from:to), VocpDPT350(from:to),'LineWidth',1.5)
-%plot(2e-10*(from:to), VrefDPT350_2(from:to),'LineWidth',1.5)
-%plot(2e-10*(from:to), VandoutDPT350(from:to),'LineWidth',1.5)
-plot(2e-10*(from:to), VcompoutSCP300(from:to),'LineWidth',1.5)
-% plot(2e-10*(from:to), VgsBotSCP300(from:to),'LineWidth',1.5)
-% plot(2e-10*(from:to), V5VISODPT320(from:to),'LineWidth',1.5)
-% plot(2e-10*(from:to), P5VISOSCP300(from:to),'LineWidth',1.5)
-% plot(2e-10*(from:to), V5VISODPT340(from:to),'LineWidth',1.5)
-% yyaxis left
-% plot(2e-10*(from:to), CommonMCurr350_ES(from:to),'LineWidth',1.5)
-% plot(2e-10*(from:to), CommonMCurr350_Adapter(from:to),'LineWidth',1.5)
+plot(time,S5V)
+plot(time,DSPSCTrig)
 ylabel('Voltage (V)')
-% ylim([-0.4 1])
-% yyaxis right
-% plot(2e-10*(from:to), P5V_wSS_350(from:to),'LineWidth',1.5)
-% ylim([0 6.5])
-% ylabel('Voltage (V)')
+%ylim([-0.4 1])
+yyaxis right
+plot(time,LoadCurrent)
+%ylim([0 6.5])
+ylabel('Current (A)')
 %plot(2e-10*(from:to), V5VnfDPT350(from:to),'LineWidth',1.5)
 %plot(2e-10*(from:to), V5VofDPT350(from:to),'LineWidth',1.5)
-legend('Reference','Filter Output','Comparator Output','5V','best')
+legend('S5V','DSP SC Input','Load Current','Location','best')
 xlabel('time (\mus)')
-title('SCP Waveforms under 300V')
+title('All isolated converters desoldered except gate drivers, 350V')
 
 
